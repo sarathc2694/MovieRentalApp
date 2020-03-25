12ca76c35d17e97c34e84f873e84d3560d21d95d
sarathchenthamarai <sarathc2694@gmail.com>
2020-03-25 10:19:22 +0530

    Initial Commit

---
+++ MovieRentalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieRentalApp
+//
+//  Created by Sarath Chenthamarai on 25/03/20.
+//  Copyright © 2020 Sarath Chenthamarai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
